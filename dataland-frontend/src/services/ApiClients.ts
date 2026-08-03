32130865a0480e21c5f737bb8eafe170434b4b35
hendrik-d-fine <hendrik.scheidel@d-fine.com>
2026-08-03 15:49:22 +0200

    Wire specification-service client into ApiClientProvider

--- dataland-frontend/src/services/ApiClients.ts
+++ dataland-frontend/src/services/ApiClients.ts
@@ -19,6 +19,7 @@ import axios, { type AxiosInstance } from 'axios';
 import { updateTokenAndItsExpiryTimestampAndStoreBoth } from '@/utils/SessionTimeoutUtils';
 import { DataPointControllerApi, DataExportControllerApi } from '@clients/backend/api';
 import { CreditsControllerApi } from '@clients/accountingservice';
+import { SpecificationControllerApi } from '@clients/specificationservice';
 
 interface ApiBackendClients {
   actuator: backendApis.ActuatorApiInterface;
@@ -43,6 +44,7 @@ interface ApiClients {
   dataExportController: DataExportControllerApi;
   companyRightsController: CompanyRightsControllerApi;
   creditsController: CreditsControllerApi;
+  specificationController: SpecificationControllerApi;
 }
 
 type ApiClientConstructor<T> = new (
@@ -110,6 +112,7 @@ export class ApiClientProvider {
       dataExportController: this.getClientFactory('/api')(DataExportControllerApi),
       companyRightsController: this.getClientFactory('/community')(CompanyRightsControllerApi),
       creditsController: this.getClientFactory('/accounting')(CreditsControllerApi),
+      specificationController: this.getClientFactory('/specifications')(SpecificationControllerApi),
     };
   }
 
